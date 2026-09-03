f97102b55db065d54e8d62fcbeb7571d51bb6879
mohanmaksud-hub <mohanmaksud@gmail.com>
2026-09-03 22:16:36 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE="digital-world-csc-v1";const A=["index.html","manifest.json","logo.jpg"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
